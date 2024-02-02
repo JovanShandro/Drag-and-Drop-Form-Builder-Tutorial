4a3899b0a66927421afdad571457316e3071e27c
JSauceda10 <j.sauceda10@yahoo.com>
2024-02-02 02:21:45 -0600

    set up main section. Need to fix font awesome icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { TutorialComponent } from './tutorial/tutorial.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { TwoListsHorizontalComponent } from './two-lists-horizontal/two-lists-horizontal.component';
+import { FieldsComponent } from './fields/fields.component';
+import { FormBuilderComponent } from './form-builder/form-builder.component';
+import { CustomizationComponent } from './customization/customization.component';
+import { FieldItemComponent } from './field-item/field-item.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -24,11 +30,18 @@ export const routes: Routes = [
     TutorialComponent,
     HomeComponent,
     TwoListsHorizontalComponent,
+    FieldsComponent,
+    FormBuilderComponent,
+    CustomizationComponent,
+    FieldItemComponent,
+    
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     DragDropModule,
+    MatTabsModule,
     RouterModule.forRoot(routes),
   ],
   providers: [],
---
+++ src/app/data/DraggableElementsList.ts
@@ -0,0 +1,54 @@
+export const DraggableElementsList = [
+    {
+        title: 'Section',
+        icon: 'fa-solid fa-table-columns',
+    },
+    {
+        title: 'Button',
+        icon: 'fa-solid fa-square',
+    },
+    {
+        title: 'Input',
+        icon: 'fa-regular fa-square',
+    },
+    {
+        title: 'Textarea',
+        icon: 'fa-regular fa-window-maximize',
+    },
+    {
+        title: 'Dropdown',
+        icon: 'fa-regular fa-caret-square-down',
+    },
+    {
+        title: 'Radio Buttons',
+        icon: 'fa-solid fa-list-ul',
+    },
+    {
+        title: 'Check Box',
+        icon: 'fa-regular fa-check-square',
+    },
+    {
+        title: 'Text',
+        icon: 'fa-solid fa-language',
+    },
+    {
+        title: 'Picture',
+        icon: 'fa-regular fa-image',
+    },
+    {
+        title: 'Spacer',
+        icon: 'fa-solid fa-arrows-alt-v',
+    },
+    {
+        title: 'Separator',
+        icon: 'fa-solid fa-grip-lines',
+    },
+    {
+        title: 'Social',
+        icon: 'fa-solid fa-share-alt',
+    },
+    {
+        title: 'Uploader',
+        icon: 'fa-solid fa-upload',
+    },
+];
