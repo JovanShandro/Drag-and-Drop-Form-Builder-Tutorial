5c69bb1ce02555ce1f9c4fefe58b6ef46e74357c
Jovan Shandro <jovan.shandro@alzura.com>
2024-02-17 19:31:04 +0100

    Add functionality to double field section

--- src/app/double-field-section/double-field-section.component.ts
+++ src/app/double-field-section/double-field-section.component.ts
@@ -1,13 +1,29 @@
 import { Component, Input } from '@angular/core';
-import { FormField, FormFieldType } from '../form.service';
+import {
+  FormField,
+  FormFieldType,
+  FormItemType,
+  FormSection,
+} from '../form.service';
+import { CdkDrag, CdkDragDrop } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-double-field-section',
   templateUrl: './double-field-section.component.html',
-  styleUrls: ['./double-field-section.component.scss']
+  styleUrls: ['./double-field-section.component.scss'],
 })
 export class DoubleFieldSectionComponent {
-  @Input() field?: FormField;
+  @Input() fields?: FormField[];
 
   formFieldType = FormFieldType;
+
+  onlyFieldsPredicate(item: CdkDrag<FormSection | FormField>) {
+    return item.data.type === FormItemType.Field;
+  }
+
+  onDrop(event: CdkDragDrop<FormField>, currentField: FormField) {
+    if (event.item.data.fieldType) {
+      currentField.fieldType = event.item.data.fieldType;
+    }
+  }
 }
