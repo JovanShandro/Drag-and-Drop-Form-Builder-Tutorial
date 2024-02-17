e5569ce4ca1c3f4465f40cd77a5b8c0b0ae6d6a1
Jovan Shandro <jovan.shandro@alzura.com>
2024-02-17 19:42:58 +0100

    Add sorting functionality for double field section

--- src/app/double-field-section/double-field-section.component.ts
+++ src/app/double-field-section/double-field-section.component.ts
@@ -5,7 +5,8 @@ import {
   FormItemType,
   FormSection,
 } from '../form.service';
-import { CdkDrag, CdkDragDrop } from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { faArrowsUpDownLeftRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-double-field-section',
@@ -13,17 +14,26 @@ import { CdkDrag, CdkDragDrop } from '@angular/cdk/drag-drop';
   styleUrls: ['./double-field-section.component.scss'],
 })
 export class DoubleFieldSectionComponent {
-  @Input() fields?: FormField[];
+  @Input() fields!: FormField[];
 
   formFieldType = FormFieldType;
+  faArrowsUpDownLeftRight = faArrowsUpDownLeftRight;
 
   onlyFieldsPredicate(item: CdkDrag<FormSection | FormField>) {
     return item.data.type === FormItemType.Field;
+  }
+
+  noEnterPredicate() {
+    return false;
   }
 
   onDrop(event: CdkDragDrop<FormField>, currentField: FormField) {
     if (event.item.data.fieldType) {
       currentField.fieldType = event.item.data.fieldType;
     }
+  }
+
+  onReorderFields(event: CdkDragDrop<FormField>) {
+    moveItemInArray(this.fields, event.previousIndex, event.currentIndex);
   }
 }
