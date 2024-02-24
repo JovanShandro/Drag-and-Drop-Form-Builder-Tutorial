c3cc47af8b1482f1dd806a182cfd8e629519efd3
Jovan Shandro <jovan.shandro@alzura.com>
2024-02-24 18:10:27 +0100

    Extract draggable section and field lists into separate components

---
+++ src/app/element-list-panel/draggable-field-list/draggable-field-list.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { faSquare as faSquareOutline } from '@fortawesome/free-regular-svg-icons';
+import {
+  faSquare as faSquareFilled,
+  faWindowMaximize,
+} from '@fortawesome/free-solid-svg-icons';
+import { FormFieldType, FormItemType, FormService } from '../../form.service';
+
+@Component({
+  selector: 'app-draggable-field-list',
+  templateUrl: './draggable-field-list.component.html',
+  styleUrls: ['./draggable-field-list.component.scss'],
+})
+export class DraggableFieldListComponent {
+  draggableFields = [
+    {
+      type: FormItemType.Field,
+      fieldType: FormFieldType.Input,
+      title: 'Input',
+      icon: faSquareOutline,
+    },
+    {
+      type: FormItemType.Field,
+      fieldType: FormFieldType.Button,
+      title: 'Button',
+      icon: faSquareFilled,
+    },
+    {
+      type: FormItemType.Field,
+      fieldType: FormFieldType.Textarea,
+      title: 'Textarea',
+      icon: faWindowMaximize,
+    },
+  ];
+
+  constructor(private formService: FormService) {}
+
+  get allFieldIds() {
+    const fieldIds: string[] = [];
+
+    this.formService.sections.forEach((section) => {
+      section.fields.forEach((field) => {
+        fieldIds.push(field.id);
+      });
+    });
+
+    return fieldIds;
+  }
+
+  noEnterPredicate() {
+    // The way how predicates work is that if this function
+    // returns true, the item will be allows to be dropped in the list,
+    // and if it returns false, it won't
+    return false;
+  }
+}
---
+++ src/app/element-list-panel/draggable-section-list/draggable-section-list.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { FormItemType, FormSectionType } from '../../form.service';
+
+@Component({
+  selector: 'app-draggable-section-list',
+  templateUrl: './draggable-section-list.component.html',
+  styleUrls: ['./draggable-section-list.component.scss'],
+})
+export class DraggableSectionListComponent {
+  draggableSections = [
+    {
+      type: FormItemType.Section,
+      sectionType: FormSectionType.OneFieldPerRow,
+      pattern: '1',
+    },
+    {
+      type: FormItemType.Section,
+      sectionType: FormSectionType.TwoFieldsPerRow,
+      pattern: '1:1',
+    },
+    {
+      type: FormItemType.Section,
+      sectionType: FormSectionType.ThreeFieldsPerRow,
+      pattern: '1:1:1',
+    },
+    {
+      type: FormItemType.Section,
+      sectionType: FormSectionType.FourFieldsPerRow,
+      pattern: '1:1:1:1',
+    },
+    {
+      type: FormItemType.Section,
+      sectionType: FormSectionType.OneToTwoPerRow,
+      pattern: '1:2',
+    },
+    {
+      type: FormItemType.Section,
+      sectionType: FormSectionType.TwoToOnePerRow,
+      pattern: '2:1',
+    },
+  ];
+
+  noEnterPredicate() {
+    // The way how predicates work is that if this function
+    // returns true, the item will be allows to be dropped in the list,
+    // and if it returns false, it won't
+    return false;
+  }
+}
