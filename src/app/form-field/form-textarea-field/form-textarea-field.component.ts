8183d1ef6d7ef299464f607a9ae827df8a52414b
Jovan Shandro <jovan.shandro@alzura.com>
2024-03-09 18:31:05 +0100

    Refactor textarea field

--- src/app/form-field/form-textarea-field/form-textarea-field.component.ts
+++ src/app/form-field/form-textarea-field/form-textarea-field.component.ts
@@ -1,18 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TextareaSettings } from 'src/app/services/form.service';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-form-textarea-field',
   templateUrl: './form-textarea-field.component.html',
   styleUrls: ['./form-textarea-field.component.scss'],
 })
-export class FormTextareaFieldComponent implements OnInit {
-
+export class FormTextareaFieldComponent {
   @Input() settings!: TextareaSettings;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  textareaId = uuid();
 }
