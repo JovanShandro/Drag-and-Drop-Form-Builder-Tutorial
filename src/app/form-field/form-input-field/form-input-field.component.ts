4f14eb3791a678e4ef1567c0d197d796e6bdc7f8
Jovan Shandro <jovan.shandro@alzura.com>
2024-03-09 10:45:23 +0100

    Refactor and style input field

--- src/app/form-field/form-input-field/form-input-field.component.ts
+++ src/app/form-field/form-input-field/form-input-field.component.ts
@@ -1,18 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { InputSettings } from 'src/app/services/form.service';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-form-input-field',
   templateUrl: './form-input-field.component.html',
   styleUrls: ['./form-input-field.component.scss'],
 })
-export class FormInputFieldComponent implements OnInit {
-
+export class FormInputFieldComponent {
   @Input() settings!: InputSettings;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  inputId = uuid();
 }
