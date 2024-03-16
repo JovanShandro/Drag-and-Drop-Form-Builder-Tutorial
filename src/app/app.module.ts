54402090bf7069a3d0c9e60fae30918bd436c18c
Jovan Shandro <jovan.shandro@alzura.com>
2024-03-16 19:22:09 +0100

    Start adding support for multiple locales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,50 +12,54 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomizationComponent } from './customization/customization.component';
 import { ButtonCustomizationComponent } from './customization/element-customization/button-customization/button-customization.component';
+import { CheckboxCustomizationComponent } from './customization/element-customization/checkbox-customization/checkbox-customization.component';
+import { DropdownCustomizationComponent } from './customization/element-customization/dropdown-customization/dropdown-customization.component';
 import { ElementCustomizationComponent } from './customization/element-customization/element-customization.component';
+import { InputCustomizationComponent } from './customization/element-customization/input-customization/input-customization.component';
+import { PictureCustomizationComponent } from './customization/element-customization/picture-customization/picture-customization.component';
+import { RadiobuttonCustomizationComponent } from './customization/element-customization/radiobutton-customization/radiobutton-customization.component';
+import { SpacerCustomizationComponent } from './customization/element-customization/spacer-customization/spacer-customization.component';
+import { TextCustomizationComponent } from './customization/element-customization/text-customization/text-customization.component';
+import { TextareaCustomizationComponent } from './customization/element-customization/textarea-customization/textarea-customization.component';
 import { DraggableFieldListComponent } from './element-list-panel/draggable-field-list/draggable-field-list.component';
 import { DraggableSectionListComponent } from './element-list-panel/draggable-section-list/draggable-section-list.component';
 import { ElementListPanelComponent } from './element-list-panel/element-list-panel.component';
 import { FieldItemComponent } from './form-builder/field-item/field-item.component';
 import { FormBuilderComponent } from './form-builder/form-builder.component';
 import { FormButtonFieldComponent } from './form-builder/form-field/form-button-field/form-button-field.component';
+import { FormCheckboxFieldComponent } from './form-builder/form-field/form-checkbox-field/form-checkbox-field.component';
+import { FormDropdownFieldComponent } from './form-builder/form-field/form-dropdown-field/form-dropdown-field.component';
 import { FormFieldComponent } from './form-builder/form-field/form-field.component';
+import { FormPictureFieldComponent } from './form-builder/form-field/form-picture-field/form-picture-field.component';
+import { FormRadiobuttonFieldComponent } from './form-builder/form-field/form-radiobutton-field/form-radiobutton-field.component';
+import { FormSpacerFieldComponent } from './form-builder/form-field/form-spacer-field/form-spacer-field.component';
+import { FormTextFieldComponent } from './form-builder/form-field/form-text-field/form-text-field.component';
 import { FormSectionComponent } from './form-builder/form-section/form-section.component';
 import { MultipleFieldSectionComponent } from './form-builder/multiple-field-section/multiple-field-section.component';
 import { SingleFieldSectionComponent } from './form-builder/single-field-section/single-field-section.component';
+import { FormInputFieldComponent } from './form-field/form-input-field/form-input-field.component';
+import { FormTextareaFieldComponent } from './form-field/form-textarea-field/form-textarea-field.component';
 import { HomeComponent } from './home/home.component';
 import { FormService } from './services/form.service';
 import { SingleListComponent } from './tutorial/single-list/single-list.component';
 import { TutorialComponent } from './tutorial/tutorial.component';
 import { TwoListsHorizontalComponent } from './tutorial/two-lists-horizontal/two-lists-horizontal.component';
 import { TwoListsComponent } from './tutorial/two-lists/two-lists.component';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { InputCustomizationComponent } from './customization/element-customization/input-customization/input-customization.component';
-import { FormInputFieldComponent } from './form-field/form-input-field/form-input-field.component';
-import { FormTextareaFieldComponent } from './form-field/form-textarea-field/form-textarea-field.component';
-import { TextareaCustomizationComponent } from './customization/element-customization/textarea-customization/textarea-customization.component';
-import { FormCheckboxFieldComponent } from './form-builder/form-field/form-checkbox-field/form-checkbox-field.component';
-import { CheckboxCustomizationComponent } from './customization/element-customization/checkbox-customization/checkbox-customization.component';
-import { FormTextFieldComponent } from './form-builder/form-field/form-text-field/form-text-field.component';
-import { TextCustomizationComponent } from './customization/element-customization/text-customization/text-customization.component';
-import { FormDropdownFieldComponent } from './form-builder/form-field/form-dropdown-field/form-dropdown-field.component';
-import { DropdownCustomizationComponent } from './customization/element-customization/dropdown-customization/dropdown-customization.component';
-import { RadiobuttonCustomizationComponent } from './customization/element-customization/radiobutton-customization/radiobutton-customization.component';
-import { FormRadiobuttonFieldComponent } from './form-builder/form-field/form-radiobutton-field/form-radiobutton-field.component';
-import { FormSpacerFieldComponent } from './form-builder/form-field/form-spacer-field/form-spacer-field.component';
-import { SpacerCustomizationComponent } from './customization/element-customization/spacer-customization/spacer-customization.component';
-import { PictureCustomizationComponent } from './customization/element-customization/picture-customization/picture-customization.component';
-import { FormPictureFieldComponent } from './form-builder/form-field/form-picture-field/form-picture-field.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'tutorial', component: TutorialComponent },
 ];
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -106,6 +113,14 @@ export const routes: Routes = [
     MatIconModule,
     NgSelectModule,
     FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [FormService],
   bootstrap: [AppComponent],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { faLanguage } from '@fortawesome/free-solid-svg-icons';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  currentLocale = 'en_US';
+  faLanguage = faLanguage;
+
+  constructor(private translateService: TranslateService) {
+    // fallback locale
+    translateService.setDefaultLang('en_US');
+
+    // current (default) locale
+    translateService.use('en_US');
+  }
+
+  languages = [
+    {
+      label: 'English',
+      value: 'en_US',
+    },
+    {
+      label: 'Espanol',
+      value: 'sp',
+    },
+  ];
+
+  updateLocale(newLocale: string) {
+    this.currentLocale = newLocale;
+    this.translateService.use(newLocale);
+  }
+}
